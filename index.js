61ba0bc25573dcf627f571fb01195eaddda8a05f
armoreDev <wiriyamu98@gmail.com>
2023-09-26 20:47:11 +0700

    Setup

--- index.js
+++ index.js
@@ -26,4 +26,7 @@ app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors);
 app.use("/assets",express.static(path.join(__dirname,'public/assets')));
+
+const PORT = process.env.PORT;
+app.listen(process.env.PORT||8880,() => console.log("Sever Start Port"+PORT));
  
