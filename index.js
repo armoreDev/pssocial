061a421517036c838889b3904cdeced7a428003b
armoreDev <wiriyamu98@gmail.com>
2023-09-26 19:48:42 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import multer from "multer";
+import morgan from "morgan";
+import path from "path";
+import helmet from "helmet"
+import { fileURLToPath } from "url";
+
+// configuration
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+
+const app = express()
+//middleware here
+
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({limit:"30mb",extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+app.use(cors);
+app.use("/assets",express.static(path.join(__dirname,'public/assets')));
+ 
