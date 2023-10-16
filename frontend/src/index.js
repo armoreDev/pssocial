11d847806bc5102f7bc49283e12b998362ae2113
armoreDev <wiriyamu98@gmail.com>
2023-10-16 17:50:46 +0700

    react persist

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  REGISTER,
+  PAUSE,
+  PURGE,
+} from "redux-persist";
+import authReducer from "./store";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
 
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: [FLUSH, REHYDRATE, REGISTER, PAUSE, PURGE],
+    });
+  },
+});
+const persistor = persistStore(store);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor} />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
---
+++ frontend/src/store/index.js
@@ -0,0 +1,60 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  mode: "light",
+  user: null,
+  profile: null,
+  token: null,
+  posts: [],
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducer: {
+    setmode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+    },
+    setLogout: (state, action) => {
+      state.user = null;
+      state.token = null;
+      state.profile = null;
+    },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.setFriends = action.payload.friends;
+      } else {
+        console.log("user friends non-exist");
+      }
+    },
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+    setPost: (state, action) => {
+      const updatePost = state.posts.map((post) => {
+        if (post._id === action.payload.post.id) {
+          return action.payload.post;
+        }
+        return post;
+      });
+      state.posts = updatePost;
+    },
+    setProfile: (state, action) => {
+      state.profile = action.payload.profile;
+    },
+  },
+});
+
+export const {
+  setmode,
+  setLogin,
+  setLogout,
+  setFriends,
+  setPosts,
+  setPost,
+  setProfile,
+} = authSlice.actions;
+
+export default authSlice.reducer;
